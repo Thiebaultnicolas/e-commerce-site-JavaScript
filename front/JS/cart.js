33b5499515d656f5780027f996f133176205ba80
Thiebaultnicolas <nicolasthiebault80870@yahoo.com>
2022-09-12 11:23:51 +0200

    test panier

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -1,3 +1,4 @@
+/*
 const cart = [];
 
 retrieveItemsFromCache () 
@@ -13,120 +14,86 @@ function retrieveItemsFromCache () {
     }
 }
 
-function displayItem(item) {
-    const article = makeArticle(item)
-    displayarticle(article)
-    console.log(article);
-    const div1 = makeImageDiv (item)
-    article.appendChild(div1)
-}
-
-function makeArticle(item) {
-    const article = document.createElement("article")
-    article.classList.add("cart__item")
-    article.dataset.id = item.id
-    article.dataset.color = item.color
-    return article
-}
-function displayarticle (article) {
-document.querySelector("#cart__items").appendChild(article)
-
-}
-function makeImageDiv(item) {
-    const div1 = document.createComment ("div")
-    div1.classList.add("cart__item__img")
-
-    const image = document.createElement("img")
-    image.src = item.imageUrl
-    image.alt = item.altTXT
-    div1.appendChild(image)
-    return div1
-}
-
-
-
-
-https://www.youtube.com/watch?v=JBqBSv5h3Ds&t=151s reprendre a 25:10
-
-
-
-
-/*
-const numberOfItems = localStorage.getItem("basket");
-console.log(localStorage.getItem("basket"));
 */
 
-
-
-
-/*
 fetch("http://localhost:3000/api/products")
 .then ((res) => res.json())
-.then((products) => displayProducts(products))
+.then((products) => displayBasketApi(products))
 .catch((error) => {
     console.error(error);
 })
 
-function displayProducts(produits) {
-    
-    const sectionCart__items = document.querySelector("#cart__items");
-    console.log(sectionCart__items);
+function displayBasketApi(produits) {
 
-    
+    const article = document.querySelector("#cart__items");
+    console.log(article);
 
     produits.forEach((produit) => {
 
-    const {_id, imageUrl, altTxt, name, description, colors ,price } = produits ;
-    console.log(_id, imageUrl, altTxt, name, description,colors,price);
-    console.log(produits);
+        const {_id, imageUrl, altTxt, name, description, colors ,price } = produits ;
+        console.log(_id, imageUrl, altTxt, name, description,colors,price);
+  
+        const article = document.createElement("article")
+        article.classList.add("cart__item")
+        article.dataset.id = _id
+        article.dataset.color = colors
 
-    const article =  document.createElement("article")
-    article.classList.add("cart__item")
-    article.dataset=`${_id}`
-    article.dataset=`${colors}`
-    console.log(article);
+        const div1 =  document.createElement("div")
+        div1.classList.add("cart__item__img")
+    
+        const image = document.createElement("img")
+        image.src = imageUrl
+        image.alt = altTxt
 
-    const div1 =  document.createElement("div")
-    div1.classList.add("cart__item__img")
+        const div2 =  document.createElement("div")
+        div2.classList.add("cart__item__content")
 
-    const image = document.createElement("img")
-    image.src = `${imageUrl}`
-    image.alt = `${altTxt}`
+        const div3 =  document.createElement("div")
+        div3.classList.add("cart__item__content_description")
 
-    const div2 =  document.createElement("div")
-    div2.classList.add("cart__item__content")
+        const nom =  document.createElement("h2")
+        nom.innerText = name
 
-    const div3 =  document.createElement("div")
-    div3.classList.add("cart__item__content_description")
+        const color =  document.createElement("p")
+        color.innerText = description
 
-    const nom =  document.createElement("h2")
-    nom.innerText = `${name}`
+        const prix =  document.createElement("p")
+        prix.innerText = price
 
-    const color =  document.createElement("p")
-    color.innerText = `${colors}`
+        const div4 =  document.createElement("div")
+        div4.classList.add("cart__item__content__settings")
+    
+        const div5 =  document.createElement("div")
+        div5.classList.add("cart__item__content__settings__quantity")
+     
+  })
+}
 
-    const prix =  document.createElement("p")
-    prix.innerText = `${price}`
+  
+  function displayBasketLocalStorage(itemLocalStorage) {
 
-    const div4 =  document.createElement("div")
-    div4.classList.add("cart__item__content__settings")
-
-    const div5 =  document.createElement("div")
-    div5.classList.add("cart__item__content__settings__quantity")
+// appeler local storage ici
+    const itemLocalStorage = localStorage.length
+    for (let i = 0; i < itemLocalStorage ;i++) {
+        const item = localStorage.getItem(localStorage.key(i))
+        const itemObject = JSON.parse(itemLocalStorage)
+        
+    }
 
     const quantity =  document.createElement("p")
-    quantity.innerText = ?;
+    //quantity.innerText = ?;
 
     const input =  document.createElement("input")
-    input.innerText = ?;
+    //input.innerText = ?;
 
     const div6 =  document.createElement("div")
     div6.classList.add("cart__item__content__settings__delete")
 
     const supprimer =  document.createElement("p")
-    supprimer.innerText = ?;
+    //supprimer.innerText = ?;
 
-
- })
 }
-*/
+
+
+
+
